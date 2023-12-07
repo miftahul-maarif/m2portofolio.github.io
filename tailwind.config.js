9bbfbb0f29fc98112bf53e5cce8bc1d4c4bd645c
miftahul-maarif <mif276212@gmail.com>
2023-12-07 23:18:43 +0700

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-
+      fontFamily: {
+        Agbalumo: "Agbalumo",
+        exo: "exo",
+      },
+      backgroundImage: {
+        'kuas': "url('../src/asset/img/kuas.png')",
+      }
     },
   },
   plugins: [],
